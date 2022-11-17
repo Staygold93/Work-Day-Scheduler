2dd7c14f8cbd08435e03fa6ee87dadb535c0c6a7
Pharez <Staygold93@github.com>
2022-11-17 01:05:19 -0500

    last commit

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
-var todaysDate = moment().format('LLLL');
+var todayDate = moment().format('LLLL');
 
-$("#currentDay").html(todaysDate);
+
+
+//--Displaying current date & time at top of page--
+$("#currentDay").html(todayDate);
 
 
 function background() {
     var currentTime = moment().hours();
-    
-    $(".time-block").each(function(){
-        var timeSlot = parseInt($(this).attr("id"));
-    //--Color will change according to current time--
-        if (timeSlot < currentTime) {
-        $(this).addClass("past");
 
-        }else if (timeSlot === currentTime) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-    
-        }else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
+
+    $(".time-block").each(function () {
+        var timeSlot = parseInt($(this).attr("id"));
+
+        if (timeSlot < currentTime) {
+            $(this).addClass("past");
+
+        } else if (timeSlot === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
         }
 
     })
 }
 
-background();
 
-$(document).ready(function(){
-    $(".saveBtn").on("click", function() {
+
+
+$(document).ready(function () {
+
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
-        localStorage.setItem(time,text);
+     
+        localStorage.setItem(time, text);
     })
 });
 
+-
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
-
-
-
-
-
-    
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-  
+background();
